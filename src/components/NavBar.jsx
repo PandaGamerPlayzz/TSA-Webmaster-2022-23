1527f29f961ead1012a0d79b22b2d512efcad821
PandaGamerPlayzz <zacharymiller515@gmail.com>
2022-11-12 14:13:24 -0500

    Added numbers and made hamburger menu close when an outside click is detected

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import { React, useEffect, useRef } from 'react';
 import { Link } from "react-router-dom";
 
 import './NavBar.css';
 
 const NavBar = () => {
+    useEffect(() => {
+        document.addEventListener("click", handleOutsideClick, true);
+    });
+
+    const menuRef = useRef(null);
+    const hamburgerRef = useRef(null);
+
     const toggleHamburger = (event) => {
         if(event.target) {
             let hamburger = event.target;
-            if(!hamburger.classList.contains("hamburger-icon-container")) hamburger = hamburger.parentNode;
+            if(hamburger.parentNode.classList.contains("hamburger-icon-container")) hamburger = hamburger.parentNode;
 
             hamburger.classList.toggle("hamburger-active");
 
@@ -19,6 +26,19 @@ const NavBar = () => {
         }
     }
 
+    const handleOutsideClick = (event) => {
+        if(!menuRef.current.contains(event.target) && !hamburgerRef.current.contains(event.target)) {
+            let hamburger = document.getElementsByClassName("hamburger-icon-container")[0];
+            hamburger.classList.remove("hamburger-active");
+
+            let menu = document.getElementById("nav-menu");
+            menu.classList.remove("menu-active");
+
+            let body = document.getElementsByTagName("body")[0];
+            body.classList.remove("disable-scroll");
+        }
+    }
+
     return (
         <header className="header">
             <nav className="nav-bar sticky">
@@ -26,12 +46,12 @@ const NavBar = () => {
                     <li className="nav-float-left"><Link id="nav-link-home" className="nav-logo" to={"/"}>Project Astra</Link></li>
                     <li className="nav-center">
                         <ul>
-                            <li className="nav-float-left"><Link id="nav-link-project-earth" to={"/project-earth"}>Project Earth</Link></li>
-                            <li className="nav-float-left"><Link id="nav-link-project-moon" to={"/project-moon"}>Project Moon</Link></li>
+                            <li className="nav-float-left"><Link id="nav-link-project-earth" to={"/project-earth"}><span>01</span> Project Earth</Link></li>
+                            <li className="nav-float-left"><Link id="nav-link-project-moon" to={"/project-moon"}><span>02</span> Project Moon</Link></li>
                         </ul>
                     </li>
                     <li className="nav-float-right">
-                        <div className="hamburger-icon-container" onClick={event => toggleHamburger(event)}>
+                        <div className="hamburger-icon-container" ref={hamburgerRef} onClick={event => toggleHamburger(event)}>
                             <div className="hamburger-icon-bar1"></div>
                             <div className="hamburger-icon-bar2"></div>
                             <div className="hamburger-icon-bar3"></div>
@@ -39,13 +59,13 @@ const NavBar = () => {
                     </li>
                 </ul>
             </nav>
-            <div id="nav-menu">
+            <div id="nav-menu" ref={menuRef}>
                 <div className="nav-menu-spacing"></div>
                 <div className="nav-menu-links">
                     <ul>
-                        <li><Link id="nav-link-project-earth" className="nav-menu-link" to={"/"}>Home</Link><hr /></li>
-                        <li><Link id="nav-link-project-earth" className="nav-menu-link" to={"/project-earth"}>Project Earth</Link><hr /></li>
-                        <li><Link id="nav-link-project-moon" className="nav-menu-link" to={"/project-moon"}>Project Moon</Link><hr /></li>
+                        <li><Link id="nav-link-project-earth" className="nav-menu-link" to={"/"}><span>00</span> Home</Link><hr /></li>
+                        <li><Link id="nav-link-project-earth" className="nav-menu-link" to={"/project-earth"}><span>01</span> Project Earth</Link><hr /></li>
+                        <li><Link id="nav-link-project-moon" className="nav-menu-link" to={"/project-moon"}><span>02</span> Project Moon</Link><hr /></li>
                     </ul>
                 </div>
             </div>       
