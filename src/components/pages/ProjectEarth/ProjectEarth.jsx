aae8afee405b40cf3d49d9fd7e516a290838cb65
PandaGamerPlayzz <zacharymiller515@gmail.com>
2022-11-11 21:08:02 -0500

    Added animations between pages

--- src/components/pages/ProjectEarth/ProjectEarth.jsx
+++ src/components/pages/ProjectEarth/ProjectEarth.jsx
@@ -5,10 +5,11 @@ import Styles from './ProjectEarth.module.css';
 
 const ProjectEarth = () => {
     return (
-        <motion.section className="content project-earth" initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}}>
-            <section id={Styles["section-1"]}>
-                <h1 id={Styles["section-1-title"]}>Project Earth</h1>
-            </section>
+        <motion.section className="content project-earth">
+            <motion.section id={Styles["section-1"]}>
+                <motion.div className={Styles["background-image"]} initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}} transition={{delay: 0.5, duration: 0.5}}></motion.div>
+                <motion.h1 id={Styles["section-1-title"]} initial={{opacity: 0}} animate={{opacity: 1, transition: {delay: 1, duration: 0.5}}} exit={{opacity: 0, transition: {duration: 0.5}}}>Project Earth</motion.h1>
+            </motion.section>
         </motion.section>
     );
 }
--- src/components/pages/ProjectMoon/ProjectMoon.jsx
+++ src/components/pages/ProjectMoon/ProjectMoon.jsx
@@ -5,10 +5,11 @@ import Styles from './ProjectMoon.module.css';
 
 const ProjectMoon = () => {
     return (
-        <motion.section className="content project-moon" initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}}>
-            <section id={Styles["section-1"]}>
-                <h1 id={Styles["section-1-title"]}>Project Moon</h1>
-            </section>
+        <motion.section className="content project-moon">
+            <motion.section id={Styles["section-1"]}>
+                <motion.div className={Styles["background-image"]} initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}} transition={{delay: 0.5, duration: 0.5}}></motion.div>
+                <motion.h1 id={Styles["section-1-title"]} initial={{opacity: 0}} animate={{opacity: 1, transition: {delay: 1, duration: 0.5}}} exit={{opacity: 0, transition: {duration: 0.5}}}>Project Moon</motion.h1>
+            </motion.section>
         </motion.section>
     );
 }
