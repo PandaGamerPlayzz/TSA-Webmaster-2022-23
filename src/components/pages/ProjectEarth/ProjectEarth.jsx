5807d38792912172060c53b9b83396ca32da0936
PandaGamerPlayzz <zacharymiller515@gmail.com>
2022-11-11 21:38:16 -0500

    Modified transition times for the h1 tags of each page

--- src/components/pages/ProjectEarth/ProjectEarth.jsx
+++ src/components/pages/ProjectEarth/ProjectEarth.jsx
@@ -7,7 +7,7 @@ const ProjectEarth = () => {
     return (
         <motion.section className="content project-earth">
             <motion.section id={Styles["section-1"]}>
-                <motion.div className={Styles["background-image"]} initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}} transition={{delay: 0.5, duration: 0.5}}></motion.div>
+                <motion.div className={Styles["background-image"]} initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}} transition={{delay: 0.25, duration: 0.5}}></motion.div>
                 <motion.h1 id={Styles["section-1-title"]} initial={{opacity: 0}} animate={{opacity: 1, transition: {delay: 1, duration: 0.5}}} exit={{opacity: 0, transition: {duration: 0.5}}}>Project Earth</motion.h1>
             </motion.section>
         </motion.section>
